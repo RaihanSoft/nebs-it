61044734819e6d5c1c8bf2462f0cb29ad67f5021
Raihan <aboraihan14@gmail.com>
2025-02-25 10:20:48 +0600

    background image added

--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -1,19 +1,26 @@
-import { Outlet } from "react-router-dom"
-import Navbar from "../Common/Navbar"
-import Footer from "../Common/Footer"
-import Banner from "../Common/Banner"
+import { Outlet } from "react-router-dom";
+import Navbar from "../Common/Navbar";
+import Footer from "../Common/Footer";
+import Banner from "../Common/Banner";
+import solar from "../assets/solar.avif"; // Import the image
 
 const MainLayout = () => {
-    return (
-        <div className="container mx-auto p-2" >
-            <Navbar />
-            <div className="flex justify-center items-center " >
-                <Banner />
-                <Outlet />
-            </div>
-            <Footer />
-        </div>
-    )
-}
+  return (
+    <div className="relative min-h-screen bg-cover bg-center bg-fixed" style={{ backgroundImage: `url(${solar})` }}>
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-black/50"></div> 
 
-export default MainLayout
+      {/* Content with z-index to appear above overlay */}
+      <div className="relative z-10">
+        <Navbar />
+        <div className="flex  justify-center items-center min-h-screen">
+          <Banner />
+          <Outlet />
+        </div>
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default MainLayout;
